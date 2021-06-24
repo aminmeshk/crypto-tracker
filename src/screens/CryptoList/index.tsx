89d4b3d5c640454000746f3911ad87b03f05c3b8
Amin Meshk <amin.meshk@gmail.com>
2021-06-24 23:21:43 +0430

    🪄 CryptoList UI done

--- src/screens/CryptoList/index.tsx
+++ src/screens/CryptoList/index.tsx
@@ -1,6 +1,11 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useCallback, useEffect} from 'react';
-import {ActivityIndicator, ListRenderItemInfo} from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  ListRenderItemInfo,
+  StatusBar,
+} from 'react-native';
 import {FlatList} from 'react-native';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
@@ -10,6 +15,8 @@ import {getMetricsStart} from '../../store/crypto/actions';
 import {CryptoState} from '../../store/crypto/types';
 import {formatNumber} from '../../utils/common';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 type NavigationProp = StackNavigationProp<RootStackParamList, 'CryptoList'>;
 
@@ -27,6 +34,7 @@ const CryptoList: React.FC<Props> = ({navigation, ...props}) => {
   const callError = useSelector(
     (state: CryptoState) => state.cryptoMetrics.error,
   );
+  const insets = useSafeAreaInsets();
 
   const dispatch = useDispatch();
 
@@ -34,9 +42,9 @@ const CryptoList: React.FC<Props> = ({navigation, ...props}) => {
     dispatch(getMetricsStart(['BTC', 'LTC', 'ETH', 'XRP']));
   }, [dispatch]);
 
-  // useEffect(() => {
-  //   dispatch(getMetricsStart());
-  // }, [dispatch]);
+  useEffect(() => {
+    callApi();
+  }, [callApi]);
 
   useEffect(() => {
     if (data) {
@@ -44,50 +52,92 @@ const CryptoList: React.FC<Props> = ({navigation, ...props}) => {
     }
   }, [data]);
 
+  const removeItem = useCallback(
+    (item: ListRenderItemInfo<MetricsResponse>) => {
+      console.log('remove item: ', item.item.data.symbol);
+    },
+    [],
+  );
+
+  const promptRemove = useCallback(
+    (item: ListRenderItemInfo<MetricsResponse>) => {
+      Alert.alert(
+        `Remove ${item.item.data.symbol}`,
+        `Are you sure about removing ${item.item.data.name}?`,
+        [
+          {
+            text: 'Remove',
+            onPress: () => removeItem(item),
+            style: 'destructive',
+          },
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+        ],
+        {
+          cancelable: true,
+        },
+      );
+    },
+    [removeItem],
+  );
+
   const renderItem = (item: ListRenderItemInfo<MetricsResponse>) => {
+    const isUp =
+      item.item.data.market_data.percent_change_usd_last_24_hours > 0;
     return (
-      <View>
-        <View style={s.row}>
-          <View style={s.nameContainer}>
-            <Text style={s.name}>{item.item.data.name}</Text>
-            <Text style={s.symbol}>{item.item.data.symbol}</Text>
+      <TouchableOpacity
+        onLongPress={() => promptRemove(item)}
+        activeOpacity={0.5}>
+        <View>
+          <View style={s.row}>
+            <View style={s.nameContainer}>
+              <Text style={s.name}>{item.item.data.name}</Text>
+              <Text style={s.symbol}>{item.item.data.symbol}</Text>
+            </View>
+            <View style={s.priceContainer}>
+              <Text style={s.name}>
+                {'$' + formatNumber(item.item.data.market_data.price_usd)}
+              </Text>
+              <Text style={[s.symbol, isUp ? s.green : s.red]}>
+                <Icon
+                  name={isUp ? 'north-east' : 'south-west'}
+                  color={isUp ? 'darkgreen' : 'darkred'}
+                />
+                {formatNumber(
+                  item.item.data.market_data.percent_change_usd_last_24_hours,
+                ) + '%'}
+              </Text>
+            </View>
           </View>
-          <View style={s.priceContainer}>
-            <Text style={s.name}>
-              {'$' + formatNumber(item.item.data.market_data.price_usd)}
-            </Text>
-            <Text
-              style={[
-                s.symbol,
-                item.item.data.market_data.percent_change_usd_last_24_hours > 0
-                  ? s.green
-                  : s.red,
-              ]}>
-              {formatNumber(
-                item.item.data.market_data.percent_change_usd_last_24_hours,
-              ) + '%'}
-            </Text>
-          </View>
+          <View style={s.seperator} />
         </View>
-        <View style={s.seperator} />
-      </View>
+      </TouchableOpacity>
     );
   };
 
   return (
-    <View style={s.screen}>
-      {isLoading && <ActivityIndicator />}
+    <View style={[s.screen, {paddingBottom: insets.bottom}]}>
+      <StatusBar barStyle="light-content" />
       {callError && <Text>{callError}</Text>}
-      <Icon name="north-east" color="black" />
       <FlatList
         data={data}
         renderItem={renderItem}
         keyExtractor={(item) => item.data.id}
         style={s.flatList}
         contentContainerStyle={s.listContainer}
+        refreshing={isLoading}
+        onRefresh={callApi}
       />
-      <Button title="Get Metrics" onPress={callApi} />
-      <Button title="Add" onPress={() => navigation.navigate('CryptoAdd')} />
+      <TouchableOpacity
+        style={s.addTouchable}
+        onPress={() => navigation.navigate('CryptoAdd')}>
+        <View style={s.buttonWrapper}>
+          <Icon name="add" color="#3b5675" style={s.addIcon} />
+          <Text style={s.addText}>Add a Cryptocurrency</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -95,7 +145,7 @@ const CryptoList: React.FC<Props> = ({navigation, ...props}) => {
 const s = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   seperator: {
     height: StyleSheet.hairlineWidth,
@@ -104,6 +154,7 @@ const s = StyleSheet.create({
   flatList: {
     // borderColor: 'red',
     // borderWidth: 1,
+    flex: 1,
   },
   listContainer: {
     paddingHorizontal: 16,
@@ -138,6 +189,21 @@ const s = StyleSheet.create({
   },
   red: {
     color: 'crimson',
+  },
+  addText: {
+    color: '#3b5675',
+  },
+  addIcon: {},
+  buttonWrapper: {
+    // borderColor: 'red',
+    // borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 12,
+  },
+  addTouchable: {
+    marginBottom: 16,
   },
 });
 
